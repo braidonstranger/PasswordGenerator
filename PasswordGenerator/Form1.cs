46c7ba517fde5a8741bfb9aff86c55ea40a86237
braidonstranger <42213983+braidonstranger@users.noreply.github.com>
2018-09-04 08:29:53 -0600

    Final V1

--- PasswordGenerator/Form1.cs
+++ PasswordGenerator/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace PasswordGenerator
 {
@@ -15,6 +16,53 @@ namespace PasswordGenerator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String pass = SHA512Converter(SHA256Converter(MD5Converter(textBox1.Text)));
+            textBox2.Text = pass;
+        }
+
+        protected String MD5Converter(String text)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+            byte[] result = md5.Hash;
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                str.Append(result[i].ToString("x2"));
+            }
+
+            return str.ToString();
+        }
+
+        protected String SHA256Converter(String text)
+        {
+            SHA256CryptoServiceProvider sha = new SHA256CryptoServiceProvider();
+            sha.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+            byte[] result = sha.Hash;
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                str.Append(result[i].ToString("x2"));
+            }
+
+            return str.ToString();
+        }
+
+        protected String SHA512Converter(String text)
+        {
+            SHA512CryptoServiceProvider sha = new SHA512CryptoServiceProvider();
+            sha.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+            byte[] result = sha.Hash;
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                str.Append(result[i].ToString("x2"));
+            }
+            return str.ToString();
         }
     }
 }
